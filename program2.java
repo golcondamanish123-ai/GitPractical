5bcb69d94d1321264b2952c42f3bcf731e48f28b
golcondamanish123-ai <golcondamanish123@gmail.com>
2026-09-06 12:47:43 +0530

    Added line

--- program2.java
+++ program2.java
@@ -2,5 +2,6 @@ class program2 {
     public static void main(String[] args) {
         int a = 10, b = 20;
         System.out.println("Sum = " + (a + b));
+        System.out.println("sum is calculated");
     }
 }
